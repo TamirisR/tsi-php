a51a6f385692e26e3a269ef38baf03c5bd5faee4
TamirisR <Tamis_rodrigues@hotmail.com>
2020-08-23 19:40:02 -0300

    Exemplo de operadores rodando com servidor web

---
+++ operadores/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$saldo = 34.5; //operador de atribuição
+
+echo "<br> Seu saldo é $saldo";
+
+$saldo = $saldo - 10; //operador de subtração
+
+echo "<br> Agora seu saldo é  $saldo ";
+
+$saldo -=10;
+
+echo "<br> Agora seu saldo é $saldo";
+
+$saldo *= 1000;//operador de multiplicação
+
+echo "<br> Agora seu saldo é $saldo";
+
