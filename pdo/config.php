43a353d80285fdf3a03682920298efc2e07d8258
TamirisR <Tamis_rodrigues@hotmail.com>
2020-11-26 01:31:16 -0300

    aula 12/11

---
+++ pdo/config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+define('DSN', 'sqlsrv:dbname=sa;host=localhost');
+define('DB_NAME', 'root1');
+define('PASS', '12345');
---
+++ pdo/db.php
@@ -0,0 +1,16 @@
+  
+<?php
+
+require_once 'config.php';
+
+
+
+try {
+    $objBanco = new PDO(DSN, DB_NAME, PASS);
+} catch (PDOException $objerro) {
+
+    echo "SGBD indisponivel : " . $objerro->getMessage();
+
+
+    exit();
+}
